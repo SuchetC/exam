6e61df3d9898c25b1522e22073926d8384c3ca76
SuchetC <suchetc0@gmail.com>
2021-10-13 17:05:40 +0530

    first commit

---
+++ submission.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+
+<?php
+<form action="display.php" method="post">
+Name of the cattle: <input type="text" name="name"><br>
+
+Tag Number: <input type="text" name="tag"><br>
+
+Gender:<select name="gender" id="gender">
+	 <option value="male">Volvo</option>
+     <option value="female">Saab</option>
+    </select><br>
+
+DOB:<input type="date" id="date" name="date"></br>
+
+cattle_img: <input type="img" name="cattle_img"></br> 
+
+
+
+<input type="submit" value="submit">
+
+<input type="reset" value="clear">
+</form>
+
+?>
+</body>
+</html>
+
+
+
+
+
+
